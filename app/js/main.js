46a7e52ca331c7141277c6cb4b1fd97f80c3bc3a
alex3959 <akmal_kamolov.tj57@bk.ru>
2021-04-15 17:47:16 +0500

    added adaptive style

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,18 @@
-$(function(){
+let menuBtn = document.querySelector('.menu__btn');
+let menuWrap = document.querySelector('.menu__list-wrap');
+let listLogo = document.querySelector('.list-logo');
+let menuCancelBtn = document.querySelector('.menu__cancel-btn');
+
+menuBtn.addEventListener('click', () => {
+    menuWrap.style = 'display: block';
+    listLogo.style = 'display: block';
+    menuCancelBtn.style = 'display: block';
+    document.body.style.overflow = 'hidden'
+})
+
+menuCancelBtn.addEventListener('click', () => {
+    menuWrap.style = '';
+    listLogo.style = '';
+    menuCancelBtn.style = '';
+    document.body.style.overflow = ''
 })
